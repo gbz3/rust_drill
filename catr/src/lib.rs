ada1dc332c7ff5666a431f9535a1c14e43b944b3
gbz3 <1808483+gbz3@users.noreply.github.com>
2024-08-04 09:26:57 +0900

    parse args

--- catr/src/lib.rs
+++ catr/src/lib.rs
@@ -1,9 +1,27 @@
 use std::error::Error;
+use clap::Parser;
 
 type MyResult<T> = Result<T, Box<dyn Error>>;
 
-pub fn run() -> MyResult<()> {
-    println!("Hello, world");
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+pub struct Config {
+    #[arg(value_name = "FILE")]
+    files: Vec<String>,
+
+    #[arg(short = 'n', long = "number", help = "number all output lines")]
+    number_lines: bool,
+
+    #[arg(short = 'b', long = "number-nonblank", help = "number nonempty output lines, overrides -n")]
+    number_nonblank_lines: bool,
+}
+
+pub fn get_args() -> MyResult<Config> {
+    Ok(Config::parse())
+}
+
+pub fn run(config: Config) -> MyResult<()> {
+    dbg!(config);
 
     Ok(())
 }
