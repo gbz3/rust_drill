cfd3ef66f74c5a8d1ad397d915e39e551557364b
gbz3 <1808483+gbz3@users.noreply.github.com>
2024-08-11 13:20:14 +0900

    args

---
+++ findr/src/lib.rs
@@ -0,0 +1,54 @@
+use std::error::Error;
+use clap::{Parser, ValueEnum};
+use clap::builder::PossibleValue;
+use regex::Regex;
+
+type MyResult<T> = Result<T, Box<dyn Error>>;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+pub struct Config {
+    #[arg(value_name = "PATH", default_value = ".",
+        help = "Search paths")]
+    paths: Vec<String>,
+
+    #[arg(short = 'n', long = "name", value_name = "NAME",
+        help = "Name")]
+    names: Vec<Regex>,
+
+    #[arg(short = 't', long = "type", value_name = "TYPE",
+    help = "Entry type")]
+    #[clap(value_enum)]
+    entry_types: Vec<EntryType>,
+}
+
+#[derive(Clone, Debug, Eq, PartialEq)]
+enum EntryType {
+    Dir,
+    File,
+    Link,
+}
+
+impl ValueEnum for EntryType {
+    fn value_variants<'a>() -> &'a [Self] {
+        &[EntryType::Dir, EntryType::File, EntryType::Link]
+    }
+
+    fn to_possible_value(&self) -> Option<PossibleValue> {
+        Some(match self {
+            EntryType::Dir => PossibleValue::new("d"),
+            EntryType::File => PossibleValue::new("f"),
+            EntryType::Link => PossibleValue::new("l"),
+        })
+    }
+}
+
+pub fn get_args() -> MyResult<Config> {
+    Ok(Config::parse())
+}
+
+pub fn run(config: Config) -> MyResult<()> {
+    println!("{:#?}", config);
+
+    Ok(())
+}
